2f9c67f7bc70fb0af7c3c2ea00ae5b293f473de4
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-09-21 17:13:18 +0530

    initial commit

---
+++ com/javapractic/anant/Arrays/RichestCustomer.java
@@ -0,0 +1,25 @@
+package com.javapractic.anant.Arrays;
+
+public class RichestCustomer {
+
+    public static void main(String[] args) {
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        System.out.println(maximumWealth(accounts));
+    }
+    public static int maximumWealth(int[][] accounts) {
+
+        int ans = Integer.MIN_VALUE;
+
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            if(sum> ans){
+                ans = sum;
+            }
+        }
+
+        return ans;
+    }
+}
