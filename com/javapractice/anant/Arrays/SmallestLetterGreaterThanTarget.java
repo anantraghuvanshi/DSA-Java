7b111babae2c28e9778480fa90ae080d285ef25c
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-09-26 20:15:49 +0530

    first occurence of first and last

--- com/javapractice/anant/Arrays/SmallestLetterGreaterThanTarget.java
+++ com/javapractice/anant/Arrays/SmallestLetterGreaterThanTarget.java
@@ -1,6 +1,6 @@
 package com.javapractice.anant.Arrays;
 
-public class SmalletLetterGreaterThanTarget {
+public class SmallestLetterGreaterThanTarget {
 
     public static void main(String[] args) {
         //some code
