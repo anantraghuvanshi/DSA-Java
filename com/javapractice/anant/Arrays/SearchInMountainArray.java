56266ebebc510beae2a339997e47c339cebb385b
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-09-28 22:05:42 +0530

    file update

---
+++ com/javapractice/anant/Arrays/SearchInMountainArray.java
@@ -0,0 +1,76 @@
+package com.javapractice.anant.Arrays;
+
+public class SearchInMountainArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int searchInMountainArray(int[] arr, int target){
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+        if(firstTry != -1){
+            return firstTry;
+        }
+        return orderAgnosticBS(arr, target, peak, arr.length-1);
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length -1;
+
+        int mid = start + (end-start)/2;
+        while(start < end){
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
+            } else  {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    static int search(int[] arr, int target, int start, int end){
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
