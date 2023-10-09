476684f46a0a39aa3e38f66050a812b9584a9787
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-10-09 14:18:12 +0530

    guess number higher or lower done

---
+++ com/javapractice/anant/Arrays/GuessNumberHigherorLower.java
@@ -0,0 +1,26 @@
+package com.javapractice.anant.Arrays;
+
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+
+public class GuessNumberHigherorLower {
+    public static void main(String[] args) {
+
+    }
+    public int guessNumber(int n) {
+        int start = 0;
+        int end = n;
+        while(start<=end){
+            int mid = start + (end - start) / 2 ;
+            if(guess(mid) == 0){
+                return mid;
+            }
+            if (guess(mid) == -1){
+                end = mid - 1;
+            }
+            if (guess(mid) == 1){
+                start =  mid + 1;
+            }
+        }
+        return -1;
+    }
+}
