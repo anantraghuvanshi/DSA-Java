3a06a3cf4ccd1259ac71b89e98df8e5a2aac806b
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-10-16 20:43:11 +0530

    commentated the error part

--- com/javapractice/anant/Searching/GuessNumberHigherorLower.java
+++ com/javapractice/anant/Searching/GuessNumberHigherorLower.java
@@ -6,21 +6,21 @@ public class GuessNumberHigherorLower {
     public static void main(String[] args) {
 
     }
-    public int guessNumber(int n) {
-        int start = 0;
-        int end = n;
-        while(start<=end){
-            int mid = start + (end - start) / 2 ;
-            if(guess(mid) == 0){
-                return mid;
-            }
-            if (guess(mid) == -1){
-                end = mid - 1;
-            }
-            if (guess(mid) == 1){
-                start =  mid + 1;
-            }
-        }
-        return -1;
+//    public int guessNumber(int n) {
+//        int start = 0;
+//        int end = n;
+//        while(start<=end){
+//            int mid = start + (end - start) / 2 ;
+//            if(guess(mid) == 0){
+//                return mid;
+//            }
+//            if (guess(mid) == -1){
+//                end = mid - 1;
+//            }
+//            if (guess(mid) == 1){
+//                start =  mid + 1;
+//            }
+//        }
+//        return -1;
     }
-}
+
