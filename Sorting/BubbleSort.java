3e5ff2c871d6b5d9680163d40679c1c84c2a77d6
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-10-16 20:45:34 +0530

    bubble sort with flag done

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -9,14 +9,18 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));;
     }
     static void bubbleSort(int[] arr){
+        boolean swapped;
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j <= arr.length - i - 1 ; j++) {
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
     }
 }
