fb425eb08ec64e6a66074c913935aa112961d702
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-10-19 16:58:12 +0530

    final value after operation done

---
+++ Strings/FinalValueOfVariable.java
@@ -0,0 +1,17 @@
+package Strings;
+
+//
+public class FinalValueOfVariable {
+    public static void main(String[] args) {
+
+    }
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if(operations[i].equals("++X") || operations[i].equals("X++") ) x++;
+            if (operations[i].equals("--X") || operations[i].equals("X--") ) x--;
+        }
+        return x;
+    }
+    }
+
